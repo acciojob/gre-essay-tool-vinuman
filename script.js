efd6330d2993e699be733d9675bb658f73f49cd7
vinu cyril <38722628+vinuman@users.noreply.github.com>
2023-03-08 14:58:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your code here
+
+const entry = document.getElementById('evaluatedText');
+const display = document.querySelector('h3');
+
+entry.addEventListener('input', function(e){
+
+	let wordCount = entry.value.trim() === "" ? 0 : entry.value.trim().split(' ').length;
+    display.textContent = wordCount;
+})
+
+
+
+
+
+
+
